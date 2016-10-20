77e6213f9d2bdc487687b313b6c332a4166e4256
Richard-Lynch <Richard@Richards-MacBook-Pro.local>
2016-10-20 12:14:58 +0100

    Setting up vars

---
+++ hash1.h
@@ -0,0 +1,17 @@
+#include "string.h"
+
+class hash_table{
+private:
+    int size;
+    int type;
+    String Search_Var;
+public:
+    void add();
+     
+
+
+
+    hash_table(int ARRAY_SIZE, type VAR_SIZE, String SEARCH_VAR);
+    ~hash_table();
+
+}
